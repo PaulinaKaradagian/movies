b68895ee99fb8ac5ce27ede2c97a1dec197c7337
Paulina Karadagian <78479614+PaulinaKaradagian@users.noreply.github.com>
2022-01-11 08:21:20 -0300

    Add files via upload

---
+++ src/router/index - copia.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import MovieDetail from '../views/MovieDetail.vue'
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: "/movie/:id",
+        name: "Movie Detail",
+        component: MovieDetail
+
+    }
+
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
